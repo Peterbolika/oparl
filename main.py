1ab854fb9fb7de9cf197cd0b73516491e164c400
Peterbolika <118926825+Peterbolika@users.noreply.github.com>
2023-02-15 19:02:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import json
 import os
 import re
 import PyPDF2
+import itertools
 
 # Liste der zu suchenden Begriffe
-regex = r"(Hauptausschuss|Rechnungsprüfungsausschuss|Wahlausschuss|Wahlprüfungsausschuss|Ausschuss für Kinder, Jugendliche und Familien|Ausschuss für Schule und Weiterbildung|Betriebsausschuss der Abfallwirtschaftsbetriebe Münster (AWM)|Betriebsausschuss citeq|Betriebsausschuss Münster Marketing|Ausschuss für Gleichstellung|Ausschuss für Personal, Digitalisierung, Organisation, Sicherheit und Ordnung|Ausschuss für Soziales, Gesundheit, Verbraucherschutz und Arbeitsförderung|Ausschuss für Stadtplanung und Stadtentwicklung|Ausschuss für Umweltschutz, Klimaschutz und Bauwesen|Ausschuss für Verkehr und Mobilität|Ausschuss für Wohnen, Liegenschaften, Finanzen und Wirtschaft|Kulturausschuss|Sportausschuss)"
+regex = r"\b(Ausschuss für Gleichstellung|Ausschuss für Personal, Digitalisierung, Organisation|Ausschuss für Soziales, Gesundheit, Verbraucherschutz und Arbeitsförderung|Ausschuss für Stadtplanung und Stadtentwicklung|Ausschuss für Umweltschutz, Klimaschutz und Bauwesen|Ausschuss für Verkehr und Mobilität|Ausschuss für Wohnen, Liegenschaften, Finanzen und Wirtschaft|Kulturausschuss|Sportausschuss)\b"
 
-pages = 1#9#80
+pages = 25# insgesamt 1980
 i = 1
 
 # Ergebnisliste
@@ -29,7 +30,6 @@ while i <= pages:
             filename = os.path.basename(data)
 
             # Setzen Sie den Pfad, unter dem die PDF-Datei gespeichert werden soll
-
             file_path = "C:/Users/pvkip/OneDrive/Desktop/Oparl/" + filename.replace(".html", ".pdf")
             #Anpassungen vornehmen! Dokument soll mit Titel abgespeichert werden
 
@@ -43,19 +43,51 @@ while i <= pages:
                 pdf_reader = PyPDF2.PdfReader(pdf_file)
 
                 # Durchsuchen des Dokuments nach Begriffen
-                search_text = ""
-                for page in pdf_reader.pages:
-                    search_text += page.extract_text()
-                if re.search(regex, search_text):
-                    print(f"{filename}: enthält Begriffe")
-                else:
-                    print(f"{filename}: enthält nicht alle Begriffe")
+                search_text = pdf_reader.pages[0].extract_text()
 
+                # Überprüfen, welcher Regex gefunden wurde
+                matches = re.findall(regex, search_text)
+                if matches:
+                    results.append((filename, matches))
 
+        a += 1
 
-        a = a+1
+    i += 1
 
-    i = i+1
+# Erstellen einer Liste aller möglichen Kombinationen von regex-Ausdrücken
+combos = list(itertools.combinations(regex.split("|"), 2))
+# Erstellen der Matrix
 
+# Extrahieren der einzigartigen Begriffe aus der Ergebnisliste
+unique_terms = list(set([term for result in results for term in result[1]]))
+
+# Erstellen der Matrix als Dictionary
+matrix = {}
+for term in unique_terms:
+    matrix[term] = [0] * len(unique_terms)
+
+# Füllen der Matrix
+for result in results:
+    for i in range(len(result[1])):
+        for j in range(i+1, len(result[1])):
+            term1 = result[1][i]
+            term2 = result[1][j]
+            if term1 != term2:
+                matrix[term1][unique_terms.index(term2)] += 1
+                matrix[term2][unique_terms.index(term1)] += 1
+
+# Ausgabe der Matrix
+header = [''] + unique_terms
+for i in range(len(unique_terms)+1):
+    for j in range(len(unique_terms)+1):
+        if i == 0 and j == 0:
+            print("{:<80}".format(header[j]), end='')
+        elif i == 0 and j != 0:
+            print("{:<80}".format(header[j]), end='')
+        elif i != 0 and j == 0:
+            print("{:<80}".format(header[i]), end='')
+        else:
+            print("{:<80}".format(matrix[unique_terms[i-1]][j-1]), end='')
+    print('\n')
 
 
